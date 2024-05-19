ebea9e9afee8420073f9c219548ef1fd9781de40
raghav1010 <raghavkol1997@gmail.com>
2024-05-19 14:56:52 +0530

    restructured the repo

--- QRCode.py
+++ QRCode.py
@@ -1,9 +1,11 @@
 import io
 import pyqrcode
-from base64 import b64encode
 import eel
 
+from base64 import b64encode
+
 eel.init('web')
+
 
 @eel.expose
 def generate_qr(data):
@@ -11,7 +13,6 @@ def generate_qr(data):
     buffers = io.BytesIO()
     img.png(buffers, scale=8)
     encoded = b64encode(buffers.getvalue()).decode("ascii")
-    print("QR code generation successful.")
     return "data:image/png;base64, " + encoded
 
 
